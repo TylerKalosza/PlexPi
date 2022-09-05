a6042c6bc19dcf9788f4950d5279978ea47ae532
Tyler Kalosza <tyler.kalosza@gmail.com>
2022-09-05 13:01:09 -0400

    Added Movies List Query

---
+++ Movies List Query.sql
@@ -0,0 +1,19 @@
+select
+	mdi.title [Title],
+	mdi.studio [Studio],
+	mdi.content_rating [Content Rating],
+	mdi.rating [Rating],
+	mdi.audience_rating [Audience Rating],
+	mi.width [Width],
+	mi.height [Height],
+	mi.container [Container],
+	mi.video_codec [Video Codec],
+	mi.audio_codec [Audio Codec]
+from
+	media_items mi
+	inner join metadata_items mdi
+		on mi.metadata_item_id = mdi.id
+where
+	mdi.metadata_type = 1
+order by
+	mdi.title_sort
